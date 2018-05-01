b284a36de357156795389bc7e02035783287cfe5
손종국 <conquerex@gmail.com>
2018-05-01 17:24:57 +0900

    Learning Hot loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
 //var React = require('react'); 와 같음
- 
+import { hot } from 'react-hot-loader';
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ''
+        }
+    }
+
     render(){
- 
+        var cnt = 0;
         return (
-                <h1>Hello React Skeleton</h1>
+            <div>
+                <button onClick={()=> {this.setState({name: 'Blah Blah'});}}>
+                    Click!!
+                </button>
+                <h1>Hello React Skeleton :::: {this.state.name}</h1>
+            </div>
         );
     }
 }
  
-export default App;
+export default hot(module)(App);
 //module.export = App; 과 같음
